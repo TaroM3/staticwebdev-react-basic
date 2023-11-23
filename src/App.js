4809d51bcaf988f702895a6bca2f88385ad13925
Lautaro Nicolas Melillo <taro.melillo@gmail.com>
2023-11-23 02:11:14 +0000

    App updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import React, { useState } from 'react';
+
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+  const [prompt, setPrompt] = useState('');
+
+  const handleImageUrlChange = (event) => {
+    setImageUrl(event.target.value);
+  };
+
+  const handlePromptChange = (event) => {
+    setPrompt(event.target.value);
+  };
+
+  const handleImageAnalysis = () => {
+    // Add your logic to trigger image analysis using the imageUrl
+    console.log('Image analysis triggered');
+  };
+
+  const handleImageGeneration = () => {
+    // Add your logic to trigger image generation using the prompt
+    console.log('Image generation triggered');
+  };
+
+  return (
+    <main>
+      <section>
+        <label>Image URL:</label>
+        <input type="text" value={imageUrl} onChange={handleImageUrlChange} />
+      </section>
+      <section>
+        <label>Prompt:</label>
+        <input type="text" value={prompt} onChange={handlePromptChange} />
+      </section>
+      <button onClick={handleImageAnalysis}>Analyze Image</button>
+      <button onClick={handleImageGeneration}>Generate Image</button>
+    </main>
+  );
 }
 
 export default App;
